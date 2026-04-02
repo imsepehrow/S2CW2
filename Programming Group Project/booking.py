367219ad2fd8fe8f1ab3d5257473d0146b4c31be
Kateryna Pakhnyts <26171953@edgehill.ac.uk>
2026-04-02 21:47:40 +0100

    Add files via upload

---
+++ Programming Group Project/booking.py
@@ -0,0 +1,38 @@
+class Booking:
+
+    def __init__(self, booking_id: str, resource, user, date: str,
+                 time_slot: str, attendees: int):
+        self._booking_id = booking_id
+        self._resource = resource
+        self._user = user
+        self._date = date
+        self._time_slot = time_slot
+        self._attendees = attendees
+
+    def get_booking_id(self) -> str:
+        return self._booking_id
+
+    def get_resource(self):
+        return self._resource
+
+    def get_user(self):
+        return self._user
+
+    def get_date(self) -> str:
+        return self._date
+
+    def get_time_slot(self) -> str:
+        return self._time_slot
+
+    def get_attendees(self) -> int:
+        return self._attendees
+# display full booking information
+    def get_booking_details(self) -> str:
+        return (
+            f"Booking ID: {self._booking_id}, "
+            f"User: {self._user.get_name()}, "
+            f"Resource: {self._resource.get_details()}, "
+            f"Date: {self._date}, "
+            f"Time Slot: {self._time_slot}, "
+            f"Attendees: {self._attendees}"
+        )
---
+++ Programming Group Project/labspace.py
@@ -0,0 +1,26 @@
+from resource import Resource
+
+# labspace inherits from resource
+class LabSpace(Resource):
+    def __init__(self, resource_id: str, location: str, max_capacity: int,
+                 number_of_pcs: int, os_type: str):
+        # call parent constructor
+        super().__init__(resource_id, location, max_capacity)
+        # private attributesspecific to labspace
+        self._number_of_pcs = number_of_pcs
+        self._os_type = os_type
+
+    def get_number_of_pcs(self) -> int:
+        return self._number_of_pcs
+
+    def get_os_type(self) -> str:
+        return self._os_type
+
+    def get_details(self) -> str:
+        return (
+            f"LabSpace ID: {self.get_resource_id()}, "
+            f"Location: {self.get_location()}, "
+            f"Capacity: {self.get_max_capacity()}, "
+            f"PCs: {self._number_of_pcs}, "
+            f"OS: {self._os_type}"
+        )
